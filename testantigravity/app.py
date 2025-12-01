c51aaf940643ab33454ce9568ecb43d440248e3a
Guilian12 <guilian.leveque@edu.univ-fcomte.fr>
2025-12-01 15:34:11 +0100

    Add intervention management routes and logic

--- testantigravity/app.py
+++ testantigravity/app.py
@@ -261,6 +261,158 @@ def valid_edit_client():
     get_db().commit()
     return redirect('/client/show')
 
+# -------------------------------------------
+# INTERVENTIONS
+# -------------------------------------------
+    
+@app.route('/interventions')
+def show_intervention():
+    mycursor = get_db().cursor()
+    sql='''
+    SELECT id_intervention, date_inter, id_pompe_a_chaleur, id_personnel, id_motif, id_client
+    FROM intervention
+    ORDER BY date_inter DESC'''
+    mycursor.execute(sql)
+    all_interventions = mycursor.fetchall()
+    return render_template('interventions/show_interventions.html', interventions=all_interventions)
+
+@app.route('/interventions/add', methods=['GET'])
+def add_intervention():
+    mycursor = get_db().cursor()
+    sql='''
+    SELECT id_modele, nom_modele
+    FROM modele'''
+    mycursor.execute(sql)
+    all_modeles= mycursor.fetchall()
+    mycursor = get_db().cursor()
+    sql='''
+    SELECT id_personnel, nom_personnel
+    FROM personnel'''
+    mycursor.execute(sql)
+    all_personnel = mycursor.fetchall()
+    mycursor = get_db().cursor()
+    sql='''
+    SELECT id_motif, libelle_motif
+    FROM motif'''
+    mycursor.execute(sql)
+    all_motifs = mycursor.fetchall()
+    mycursor = get_db().cursor()
+    sql='''
+    SELECT id_client, nom_client, prenom_client
+    FROM client'''
+    mycursor.execute(sql)
+    all_clients = mycursor.fetchall()
+    return render_template('interventions/add_interventions.html', modeles=all_modeles,personnel=all_personnel,motifs=all_motifs,clients=all_clients)
+
+@app.route('/interventions/add', methods=['POST'])
+def valid_add_intervention():
+    date = request.form.get('date')
+    modele = request.form.get('modele')
+    personnel = request.form.get('personnel')
+    motif = request.form.get('motif')
+    client = request.form.get('client')
+    mycursor = get_db().cursor()
+    sql='''
+    INSERT INTO intervention(id_intervention, date_inter, id_pompe_a_chaleur, id_personnel, id_motif, id_client)
+    VALUES (NULL, %s, %s, %s, %s, %s);'''
+    tuple_param=(date, modele, personnel, motif, client)
+    mycursor.execute(sql,tuple_param)
+    get_db().commit()
+    message = u'Intervention ajoutée. Date : '+date+ ' / Modèle : ' + modele + ' / Personnel : ' + personnel + ' / Motif : '+  motif + ' / Client : ' + client
+    flash(message, 'alert-success')
+    return redirect('/interventions')
+
+@app.route('/inerventions/edit', methods=['GET'])
+def edit_intervention():
+    mycursor = get_db().cursor()
+    id=request.args.get('id')
+    if id != None and id.isnumeric():
+        indice = int(id)
+        sql='''
+        SELECT *
+        FROM intervention
+        WHERE id_intervention=%s'''
+        mycursor.execute(sql,indice)
+        inter_cible=mycursor.fetchone()
+    else:
+        inter_cible=None
+    sql = '''
+        SELECT id_modele, nom_modele
+        FROM modele'''
+    mycursor.execute(sql)
+    all_modeles = mycursor.fetchall()
+    print(all_modeles)
+    sql = '''
+        SELECT id_personnel, nom_personnel
+        FROM personnel'''
+    mycursor.execute(sql)
+    all_personnel = mycursor.fetchall()
+    sql = '''
+        SELECT id_motif, libelle_motif
+        FROM motif'''
+    mycursor.execute(sql)
+    all_motifs = mycursor.fetchall()
+    sql = '''
+        SELECT id_client, nom_client, prenom_client
+        FROM client'''
+    mycursor.execute(sql)
+    all_clients = mycursor.fetchall()
+    return render_template('interventions/edit_interventions.html', intervention=inter_cible, modeles=all_modeles,personnel=all_personnel,motifs=all_motifs,clients=all_clients)
+
+@app.route('/interventions/edit', methods=['POST'])
+def valid_edit_intervention():
+    mycursor = get_db().cursor()
+    id = request.form.get('id')
+    date = request.form.get('date')
+    modele = request.form.get('modele')
+    personnel = request.form.get('personnel')
+    motif = request.form.get('motif')
+    client = request.form.get('client')
+    sql = '''
+            UPDATE intervention
+            SET date_inter = %s,
+                id_pompe_a_chaleur = %s,
+                id_personnel = %s,
+                id_motif = %s,
+                id_client = %s
+            WHERE id_intervention = %s
+          '''
+    valeurs = (date, modele, personnel, motif, client, id)
+    mycursor.execute(sql, valeurs)
+    get_db().commit()
+    message = u'Intervention modifiée. ID : '+ id+' / Date : ' + date + ' / Modèle : ' + modele + ' / Personnel : ' + personnel + ' / Motif : ' + motif + ' / Client : ' + client
+    flash(message, 'alert-success')
+    return redirect('/interventions')
+
+@app.route('/interventions/delete')
+def delete_intervention():
+    mycursor = get_db().cursor()
+    id=request.args.get('id')
+    sql = '''
+    DELETE FROM intervention
+    WHERE id_intervention = %s;'''
+    mycursor.execute(sql, id)
+    get_db().commit()
+    return redirect('/interventions')
+
+# -------------------------------------------
+# STATISTIQUES DES INTERVENTIONS
+# -------------------------------------------
+
+@app.route('/interventions/etats')
+def show_etat_interventions():
+    mycursor = get_db().cursor()
+    sql = '''
+        SELECT personnel.id_personnel, AVG(intervention.date_inter) AS moyen_date, personnel.nom_personnel
+        FROM personnel
+        JOIN intervention ON intervention.id_personnel = personnel.id_personnel
+        GROUP BY personnel.id_personnel, personnel.nom_personnel
+        ORDER BY moyen_date DESC;
+    '''
+    mycursor.execute(sql)
+    stats = mycursor.fetchall()
+    print(stats)
+    return render_template('/interventions/etats_interventions.html', stats=stats)
 
 # -------------------------------------------
 # ETATS / STATISTIQUES
